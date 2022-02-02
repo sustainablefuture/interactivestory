2cd9a2f701f7ddea8827ed808811d47eb0364d80
Guillaume Belanger Brie <gbelangerbrie@gmail.com>
2022-02-02 11:57:42 +0100

    Add decoratiuon

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import CharacterPortrait from "./Components/CharacterPortrait";
 import Companion from "./Components/Companion";
 import MainContent from "./Components/MainContent";
 import useToggle from "./hooks/useToggle";
+import Desk from "./images/desk5.jpg";
 
 function App() {
   const [menu, toggleMenu] = useToggle(false);
 
   return (
-    <div className="App">
+    <div
+      style={{
+        backgroundImage: `url(${Desk})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+      className="App"
+    >
       <div className="left-container">
         <MainContent />
         <div className="bottom-left">
